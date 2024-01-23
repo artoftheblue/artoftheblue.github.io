09166d969cd87b8ee587f1479fbdd6e3ee94c889
Artyom Sinelnikov <avsinelnikov@edu.hse.ru>
2024-01-24 02:44:39 +0300

    color update

--- plugin.mjs
+++ plugin.mjs
@@ -8,12 +8,12 @@ function span(value, style) {
 }
 
 const styles = [
-  { name: 'remove', style: { color: 'red', textDecoration: 'line-through' } },
-  { name: 'add', style: { color: 'green', fontWeight: 'bold' } },
-  { name: 'omit', style: { color: 'blue', textDecoration: 'line-through'} },
-  { name: 'clarify', style: { color: 'blue', fontWeight: 'bold'} },
-  { name: 'ambiguous', style: { color: 'yellow', fontWeight: 'bold'} },
-  { name: 'highlight', style: { color: 'purple', fontWeight: 'bold'} }
+  { name: 'remove', style: { color: '#FDEBEC', textDecoration: 'line-through' } },
+  { name: 'add', style: { color: '#EDF3EC', fontWeight: 'bold' } },
+  { name: 'omit', style: { color: '#E7F3F8', textDecoration: 'line-through'} },
+  { name: 'clarify', style: { color: '#E7F3F8', fontWeight: 'bold'} },
+  { name: 'ambiguous', style: { color: '#FBF3DB', fontWeight: 'bold'} },
+  { name: 'highlight', style: { color: '#F6F3F9', fontWeight: 'bold'} }
 ];
 
 const directives = [];
